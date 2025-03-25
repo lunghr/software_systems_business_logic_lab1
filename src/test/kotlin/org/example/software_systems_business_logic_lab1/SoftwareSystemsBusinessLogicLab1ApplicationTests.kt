ecd198b63b707069db68c2c778aab9eca250120a
lunghr <myilanka8@gmail.com>
2025-03-25 19:43:57 +0300

    feat: initial

---
+++ src/test/kotlin/org/example/software_systems_business_logic_lab1/SoftwareSystemsBusinessLogicLab1ApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.example.software_systems_business_logic_lab1
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SoftwareSystemsBusinessLogicLab1ApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
